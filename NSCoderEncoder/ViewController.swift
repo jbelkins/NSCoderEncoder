51c1ea5e952764e5a9f7168d8acb82b72b2c26b9
Josh Elkins <jelkins@parkwhiz.com>
2018-10-07 11:44:53 -0500

    Initial Commit

---
+++ NSCoderEncoder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NSCoderEncoder
+//
+//  Created by Josh Elkins on 10/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
